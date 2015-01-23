0c823419c28da988ff5910c6c13c880028d4fd1b
Yuri Shilka <shilka@profit-partner.ru>
2015-01-24 02:57:25 +0300

    Подключил download.js, +небольшой фикс

--- _dev/_js/download.js
+++ _dev/_js/download.js
@@ -3,8 +3,10 @@ $(function () {
 
   console.log('downloader init');
 
-  $('.form__download-button').click(function(e) {
+  $('.form__button_submit').click(function(e) {
     e.preventDefault();
+
+    console.log('form__download_button click');
 
     var $originalEl = $('.picture__upload');
     var $waterEl = $('.picture__watermark');
