ec6a707083a0729381a20e7c05d7397f478f1515
Yuri Shilka <yuri.shilka@gmail.com>
2015-01-27 01:33:41 +0300

    Добавил скачивание вотермарка в режиме "плитка"

--- _dev/_server/ajax/download.php
+++ _dev/_server/ajax/download.php
@@ -3,7 +3,7 @@
 namespace Firstwatermark;
 
 class ImageConverter {
-  public function convert($source, $watermark, $x, $y, $opacity) {
+  public function convert($source, $watermark, $x, $y, $opacity, $tileMode, $marginX, $marginY) {
 
     // 0. Убедимся, что файлы существуют и получим их размеры
     $source = PATH_BASE . '/uploads/' . $source;
@@ -21,9 +21,14 @@ class ImageConverter {
     list($w2, $h2, , ) = getimagesize($watermark);
 
     // 1. Создадим результирующий пустой холст, на котором будем рисовать
-    $newWidth = $w1 + $w2;
+    $newWidth = max($w1, $w2);
     $newHeight = max($h1, $h2);
     $newImage = imagecreatetruecolor($newWidth, $newHeight);
+
+    if(!$newImage) {
+      throw new \RuntimeException('Image creation failed.');
+    }
+
     imagealphablending($newImage, true); // что-то как-то связанное с альфа каналами
     imagesavealpha($newImage, true);     // в такой комбинации работает
 
@@ -63,8 +68,20 @@ class ImageConverter {
 
     // 2. формируем новое изображение - копируем исходные изображения
     // в нужные позиции
+
     imagecopyresampled($newImage, $sourceImg, 0, 0, 0, 0, $w1, $h1, $w1, $h1);
-    imagecopyresampled($newImage, $waterImg, $x, $y, 0, 0, $w2, $h2, $w2, $h2);
+
+    // в зависимости от режима плитки копируем один вотермарк или создаем плитку
+    if($tileMode == 'single') {
+      imagecopyresampled($newImage, $waterImg, $x, $y, 0, 0, $w2, $h2, $w2, $h2);
+    } else {
+      // кто-нибудь, объясните мне, как это, вот это внизу, могло произойти?
+      for($i = $x; $i < $w1 + $x + $marginX * ($w1 / ($w2 + $marginX)); $i += $w2 + $marginX) {
+        for($j = $y; $j < $h1 + $y; $j += $h2 + $marginY) {
+          imagecopyresampled($newImage, $waterImg, $i, $j, 0, 0, $w2, $h2, $w2, $h2);
+        }
+      }
+    }
 
     header("Content-Type: application/stream");
     header("Content-Disposition: attachment; filename=result.png");
@@ -96,8 +113,11 @@ try {
   $scaleY = round((float)$_GET['scaleY'], 2);
   $x = (int)$_GET['x'] * $scaleX;
   $y = (int)$_GET['y'] * $scaleY;
+  $tileMode = ($_GET['tileMode'] == 'grid') ? 'grid' : 'single';
+  $marginX = (int)$_GET['marginX'];
+  $marginY = (int)$_GET['marginY'];
 
-  ImageConverter::convert($source, $watermark, $x, $y, $opacity);
+  ImageConverter::convert($source, $watermark, $x, $y, $opacity, $tileMode, $marginX, $marginY);
 
 } catch (\Exception $e) {
 
