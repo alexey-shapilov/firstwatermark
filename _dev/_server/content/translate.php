a46c867f2747512b3c4fea3a7471ba9de6be7571
alexey-shapilov <alexey.shapilov@gmail.com>
2015-01-26 15:12:13 +0300

    Изменил объявление массива translate.php для версий PHP ниже 5.5

--- _dev/_server/content/translate.php
+++ _dev/_server/content/translate.php
@@ -1,20 +1,21 @@
 <?php
 
-$strings = [
-  'eng' => [
-    'Генератор водяных знаков' => 'Watermark generator',
-    'Настройки' => 'Settings',
-    'Исходное изображение' => 'Source image',
-    'Водяной знак' => 'Watermark',
-    'Положение' => 'Position',
-    'Прозрачность' => 'Opacity',
-    'Сброс' => 'Reset',
-    'Скачать' => 'Download'
-  ]
-];
+$strings = array(
+    'eng' => array(
+        'Генератор водяных знаков' => 'Watermark generator',
+        'Настройки' => 'Settings',
+        'Исходное изображение' => 'Source image',
+        'Водяной знак' => 'Watermark',
+        'Положение' => 'Position',
+        'Прозрачность' => 'Opacity',
+        'Сброс' => 'Reset',
+        'Скачать' => 'Download'
+    )
+);
 
-function t($string) {
-  global $strings, $lang;
+function t($string)
+{
+    global $strings, $lang;
 
-  return $strings[$lang][$string] ?: $string;
+    return $strings[$lang][$string] ?: $string;
 }
