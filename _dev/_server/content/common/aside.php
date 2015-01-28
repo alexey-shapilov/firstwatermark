02dc998e57e0c3672c4368857053e9054803cb32
alexey-shapilov <alexey.shapilov@gmail.com>
2015-01-28 18:08:44 +0300

    Добавил анимационный gif на время загрузки изображения

--- _dev/_server/content/common/aside.php
+++ _dev/_server/content/common/aside.php
@@ -14,9 +14,9 @@
     <div class="aside__coll">
         <div class="social">
             <ul class="social__list">
-                <li class="social__item"><a href="#" class="social__link social__item_facebook"><span class="social__text">facebook</span></a></li>
-                <li class="social__item"><a href="#" class="social__link social__item_twitter"><span class="social__text">twitter</span></a></li>
-                <li class="social__item"><a href="#" class="social__link social__item_vkontakte"><span class="social__text">vkontakte</span></a></li>
+                <li class="social__item"><a href="http://www.facebook.com/sharer/sharer.php?s=100&p%5Btitle%5D=Сервис водяных знаков&p%5Bsummary%5D=Сервис позволяющий установить водяной знак на изображение&p%5Burl%5D=http://watermark.zz.mu&p%5Bimages%5D%5B0%5D=http://watermark.zz.mu/css/img/common_bg.jpg"  target="_blank" onclick="return Share.me(this);" class="social__link social__item_facebook"><span class="social__text">facebook</span></a></li>
+                <li class="social__item"><a href="https://twitter.com/intent/tweet?original_referer=http://watermark.zz.mu&text=Сервис водяных знаков&url=http://watermark.zz.mu"  target="_blank" onclick="return Share.me(this);" class="social__link social__item_twitter"><span class="social__text">twitter</span></a></li>
+                <li class="social__item"><a href="http://vk.com/share.php?url=http://watermark.zz.mu&title=Сервис водяных знаков&description=Сервис позволяющий установить водяной знак на изображение&image=http://watermark.zz.mu/css/img/common_bg.jpg&noparse=true" target="_blank" onclick="return Share.me(this);" class="social__link social__item_vkontakte"><span class="social__text">vkontakte</span></a></li>
             </ul><a href="#" class="social__head"></a>
         </div>
     </div>
