9c77ce16742f771ad3f5b8c576769aa16352c1e8
jobsboris27 <jobsboris27@gmail.com>
2015-01-30 16:23:24 +0300

    Попытка причесать uploader и reseBtn

--- _dev/_js/resetBtn.js
+++ _dev/_js/resetBtn.js
@@ -3,12 +3,13 @@
         resetBtn = $('.form__button_reset');
 
     resetBtn.on('click', function (event) {
-        var
-            position = uploader.getPosition();
+		position = uploader.getPosition();
+
         event.preventDefault();
         if (position !== undefined) {
             slider.$rangeOpacity.slider('value', 0.5);
-            position.init();
+            uploader.positionToogle(false);
+
         }
     })
 
--- _dev/_js/uploader.js
+++ _dev/_js/uploader.js
@@ -1,31 +1,106 @@
-var uploader = (function ($) {
+function Uploader(options) { //конструктор загрузчика
 
-    var
-        self = this,
-        $uploadWatermark = $('#upload_watermark'),
-        $uploadPicture = $('#upload_picture');
+	this.$uploadWatermark = options.$uploadWatermark;
+    this.$uploadPicture = options.$uploadPicture;
+}
+
+Uploader.prototype.init = function () {
+    
+	self = this;
+	this.url = '/uploader.ajax';
+	this.scale = {
+		x: 1,
+		y: 1
+	};
+	this.watermarkSizeOriginal = {
+		width: 0,
+		height: 0
+	};
+	this.pictureWorkspaceClass = 'picture__workspace',
+	this.position = 0;
 
     this.step = {
         secondElem: $('.upload__item'),
         thirdElem: $('.place'),
         thirdElemTransparent: $('.transparent')
     };
+	
+	this.uploadPictureActive(); // вызываем метод загрузки картинки у конструктора
+    this.uploadWatermarkActive(); // вызываем метод загрузки водяного знака у конструктора
+	
+	// используется в download.js для получения текущего масштаба
 
-    var url = '/uploader.ajax',
-        scale = {
-            x: 1,
-            y: 1
-        },
-        watermarkSizeOriginal = {
-            width: 0,
-            height: 0
-        },
-        pictureWorkspaceClass = 'picture__workspace',
-        position;
 
-    //step 1
+	this.getScale = function () {
+		return self.scale;
+	};
+	this.getPosition = function () {
+		return self.position;
+	};
+
+};
+
+Uploader.prototype.positionToogle = function (val) {
+
+	if(val){
+		self.position = new Position({
+
+			//x and Y value, button
+			axisButtons: {
+				x: {
+					input: $('#x_coordinate'),
+					inputTitle: $('.coord__title_x'),
+					btnUp: $('#x_coordinate_up'),
+					btnDown: $('#x_coordinate_down')
+				},
+				y: {
+					input: $('#y_coordinate'),
+					inputTitle: $('.coord__title_y'),
+					btnUp: $('#y_coordinate_up'),
+					btnDown: $('#y_coordinate_down')
+				},
+				writeCoord: function (coord, input) {
+					input.val(Math.round(coord));
+				}
+			},
+
+			//area elem
+			$workspace: $('.picture__workspace'),
+			$mainImg: $('.picture__upload'),
+
+			//elem
+			$watermark: $('.picture__watermark'),
+
+			// place grid events
+			$placeGrid: $('.grid'),
+
+			//position grid buttons
+			gridButtons: 'grid__item',
+
+			//mosh buttons
+
+			switchBtn: {
+				tileBtn: $('.toggle__item_grid'),
+				singleBtn: $('.toggle__item_single')
+			},
+
+			tileGridCross: $('.grid__closed'),
+
+			slider: slider
+		});
+
+		self.position.init();
+	}else{
+		self.position.init();
+	}
+};
+
+Uploader.prototype.uploadPictureActive = function () {
+	var
+        self = this;
+	//step 1
     //add text src img
-    $uploadPicture.change(function () {
+    self.$uploadPicture.change(function () {
         var valueFile = $(this).val();
         $(this).next().text(valueFile);
         self.step.secondElem.eq(1).removeClass('opacity__disabled');
@@ -33,14 +108,16 @@ var uploader = (function ($) {
     });
 
 
-    $uploadPicture.fileupload({
-        url: url,
+    self.$uploadPicture.fileupload({
+        url: self.url,
         dataType: 'json',
 
         change: function (e, data) {
             $.each(data.files, function (index, file) {
                 $('.upload__field-source').html(file.name);
             });
+			console.log(self.url);
+				
         },
         send: function () {
             console.log('send');
@@ -49,7 +126,7 @@ var uploader = (function ($) {
         done: function (e, data) {
             var pic = $('.picture__upload'),
                 workspace = $('.picture__images'),
-                picture__workspace = $('.' + pictureWorkspaceClass),
+                picture__workspace = $('.' + self.pictureWorkspaceClass),
                 picSizeOriginal = {
                     width: 0,
                     height: 0
@@ -62,7 +139,7 @@ var uploader = (function ($) {
                 pic.on('load', (function () {
                         console.log('сработало событие pic.load');
                         var $this = $(this),
-                            watermark = $('.' + pictureWorkspaceClass + ' > .picture__watermark');
+                            watermark = $('.' + self.pictureWorkspaceClass + ' > .picture__watermark');
                         $('.picture__uploading').css('display', 'none');
                         console.log('картинка: ', $this.width(), ' ', $this.height());
                         picSizeOriginal.width = $this.width();
@@ -71,22 +148,22 @@ var uploader = (function ($) {
                         $this.attr('style', 'max-width:' + workspace.width() + 'px;' + 'max-height:' + workspace.height() + 'px;');
                         console.log('картинка после масштабирования: ', $this.width(), ' ', $this.height());
                         picture__workspace.attr('style', 'width:' + $this.width() + 'px;' + 'height:' + $this.height() + 'px;');
-                        scale.x = picSizeOriginal.width / $this.width();
-                        scale.y = picSizeOriginal.height / $this.height();
-                        console.log('масштаб: ', scale);
+                        self.scale.x = picSizeOriginal.width / $this.width();
+                        self.scale.y = picSizeOriginal.height / $this.height();
+                        console.log('масштаб: ', self.scale);
                         pic.show();
 
                         console.log('водяной знак src: ', watermark.attr('src'));
                         if (watermark.attr('src')) {
                             watermark.hide();
                             watermark.attr('style', '');
-                            console.log('водяной знак: ', watermarkSizeOriginal.width, ' ', watermarkSizeOriginal.height);
-                            console.log('водяной знак должен стать: ', watermarkSizeOriginal.width / scale.x, ' ', watermarkSizeOriginal.height / scale.y);
-                            watermark.attr('style', 'width:' + watermarkSizeOriginal.width / scale.x + 'px;' + 'height:' + watermarkSizeOriginal.height / scale.y + 'px;');
+                            console.log('водяной знак: ', self.watermarkSizeOriginal.width, ' ', self.watermarkSizeOriginal.height);
+                            console.log('водяной знак должен стать: ', self.watermarkSizeOriginal.width / self.scale.x, ' ', self.watermarkSizeOriginal.height / self.scale.y);
+                            watermark.attr('style', 'width:' + self.watermarkSizeOriginal.width / self.scale.x + 'px;' + 'height:' + self.watermarkSizeOriginal.height / self.scale.y + 'px;');
                             console.log('водяной знак после масштабирования: ', watermark.width(), ' ', watermark.height());
                             watermark.show();
                             slider.sliderOpacity(slider.$rangeOpacity.slider("value"));
-                            position.init();
+                            self.positionToogle(false);
                         }
                         $this.off('load');
                     })
@@ -97,18 +174,22 @@ var uploader = (function ($) {
         }
 
     });
+};
 
-    //step 2
-    $uploadWatermark.attr('disabled', 'disabled');
-    this.step.secondElem.eq(1).addClass('opacity__disabled');
-    this.step.thirdElem.addClass('opacity__disabled');
-    this.step.thirdElem.append('<div class="opacity__disabled__block"></div>')
-    this.step.thirdElemTransparent.addClass('opacity__disabled');
+Uploader.prototype.uploadWatermarkActive = function () {
+	var
+        self = this;
+	//step 2
+    self.$uploadWatermark.attr('disabled', 'disabled');
+    self.step.secondElem.eq(1).addClass('opacity__disabled');
+    self.step.thirdElem.addClass('opacity__disabled');
+    self.step.thirdElem.append('<div class="opacity__disabled__block"></div>')
+    self.step.thirdElemTransparent.addClass('opacity__disabled');
     $('.transparent__item').slider('disable');
 
     //step 3
     //add text src watermark
-    $uploadWatermark.change(function () {
+    self.$uploadWatermark.change(function () {
         var valueFile = $(this).val();
         console.log(valueFile);
         $(this).next().text(valueFile);
@@ -117,10 +198,9 @@ var uploader = (function ($) {
         $('.opacity__disabled__block').remove();
         $('.transparent__item').slider('enable');
     });
-
-
-    $uploadWatermark.fileupload({
-        url: url,
+	
+	self.$uploadWatermark.fileupload({
+        url: self.url,
         dataType: 'json',
 
         change: function (e, data) {
@@ -134,7 +214,7 @@ var uploader = (function ($) {
         },
         done: function (e, data) {
             if (data.result.src) {
-                var watermark = $('.' + pictureWorkspaceClass + ' > .picture__watermark');
+                var watermark = $('.' + self.pictureWorkspaceClass + ' > .picture__watermark');
                 watermark.attr('style', '');
                 watermark.hide();
                 watermark.attr('src', data.result.src);
@@ -143,66 +223,20 @@ var uploader = (function ($) {
                             $this = $(this);
                         $('.picture__uploading').css('display', 'none');
                         console.log('водяной знак: ', $this.width(), ' ', $this.height());
-                        watermarkSizeOriginal.width = $this.width();
-                        watermarkSizeOriginal.height = $this.height();
-                        console.log('водяной знак должен стать: ', $this.width() / scale.x, ' ', $this.height() / scale.y);
-                        $this.attr('style', 'opacity:0.5; width:' + watermarkSizeOriginal.width / scale.x + 'px;' + 'height:' + watermarkSizeOriginal.height / scale.y + 'px;');
+                        self.watermarkSizeOriginal.width = $this.width();
+                        self.watermarkSizeOriginal.height = $this.height();
+                        console.log('водяной знак должен стать: ', $this.width() / self.scale.x, ' ', $this.height() / self.scale.y);
+                        $this.attr('style', 'opacity:0.5; width:' + self.watermarkSizeOriginal.width / self.scale.x + 'px;' + 'height:' + self.watermarkSizeOriginal.height / self.scale.y + 'px;');
                         console.log('водяной знак после масштабирования: ', $this.width(), ' ', $this.height());
 
                         $this.show();
                         slider.sliderOpacity(slider.$rangeOpacity.slider('value'));
                         $this.off('load');
-
-                        if (position === undefined) {
-                            position = new Position({
-
-                                //x and Y value, button
-                                axisButtons: {
-                                    x: {
-                                        input: $('#x_coordinate'),
-                                        inputTitle: $('.coord__title_x'),
-                                        btnUp: $('#x_coordinate_up'),
-                                        btnDown: $('#x_coordinate_down')
-                                    },
-                                    y: {
-                                        input: $('#y_coordinate'),
-                                        inputTitle: $('.coord__title_y'),
-                                        btnUp: $('#y_coordinate_up'),
-                                        btnDown: $('#y_coordinate_down')
-                                    },
-                                    writeCoord: function (coord, input) {
-                                        input.val(Math.round(coord));
-                                    }
-                                },
-
-                                //area elem
-                                $workspace: $('.picture__workspace'),
-                                $mainImg: $('.picture__upload'),
-
-                                //elem
-                                $watermark: $('.picture__watermark'),
-
-                                // place grid events
-                                $placeGrid: $('.grid'),
-
-                                //position grid buttons
-                                gridButtons: 'grid__item',
-
-                                //mosh buttons
-
-                                switchBtn: {
-                                    tileBtn: $('.toggle__item_grid'),
-                                    singleBtn: $('.toggle__item_single')
-                                },
-
-                                tileGridCross: $('.grid__closed'),
-
-                                slider: slider
-                            });
-
-                            position.init();
+						console.log(self.position);
+                        if (self.position === 0) {
+                            self.positionToogle(true);
                         } else {
-                            position.init();
+                           self.positionToogle(false);
                         }
                     }
                 );
@@ -213,18 +247,10 @@ var uploader = (function ($) {
             }
         }
     });
-
-
-    // используется в download.js для получения текущего масштаба
-    return {
-
-        getScale: function () {
-            return scale;
-        },
-        getPosition: function () {
-            return position;
-        }
-
-    };
-
-})(jQuery);
+	
+};
+var uploader = new Uploader({// указываем ему необходимые элементы для работы
+    $uploadWatermark: $('#upload_watermark'),
+    $uploadPicture: $('#upload_picture')
+}); //создаем наш загрузчик
+uploader.init();
